7f93a7e0fa99bd34e16f155a22231386083a362a
Jared Guyer <jaredg8@gmail.com>
2017-11-13 12:44:52 +0100

    add updated logic

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,11 @@
 class QuestionsController < ApplicationController
   def answer
     @query = params[:query]
+    @answer = if @query.downcase.include? "work"
+      "good boy!"
+    else
+      ["You are a dummy", "go to church", "I like big butts and I cannot lie!"].sample
+    end
 
   end
 
